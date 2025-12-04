d19c2b7f45bcbdc62da3bfcb2a94c7db7c87e5eb
Hari Priya <haripriya@Haris-MacBook-Air.local>
2025-12-04 16:29:07 +0530

    changed the port

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use('/api/audio', audioRoutes);
 
 // Start the server
-app.listen(5000, () => {
-  console.log(`Server is running on port ${5000}`);
+app.listen(8000, () => {
+  console.log(`Server is running on port ${8000}`);
 });
 
 export default app;
